d2ebab6c69c3144190962a4cc7e2dabca0ac1194
zakiyznw <zakiynashrudin1st@gmail.com>
2025-09-14 14:45:04 +0700

    Complete tugas Individu 3

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_json_by_id, show_main, show_product, show_xml, show_json, show_xml_by_id, show_json_by_id, add_product, rate_product, add_stock, reduce_stock, get_products_json
+from main.views import show_json_by_id, show_main, show_product, show_xml, show_json, show_xml_by_id, show_json_by_id, add_product, rate_product, add_stock, reduce_stock
 
 app_name = "main"
 
@@ -14,5 +14,4 @@ urlpatterns = [
     path("products/xml/<str:product_id>/", show_xml_by_id, name="show_xml_by_id_products"),
     path("products/json/<str:product_id>/", show_json_by_id, name="show_json_by_id_products"),
     path("add_product/", add_product, name="add_product"),
-    path("products/json_ajax/", get_products_json, name="products_json_ajax"),
 ]
--- static/js/base.js
+++
@@ -1,9 +0,0 @@
-function scrollToShop(){
-  document.getElementById('shop').scrollIntoView({behavior:'smooth'});
-}
-
-function toggleNav(){
-  const nl = document.getElementById('navLinks');
-  nl.style.display = (nl.style.display === 'flex') ? 'none' : 'flex';
-  nl.style.flexDirection = 'column';
-}
