84c502dd6d78e328295c60810494ab73a3dc4f24
zakiyznw <zakiynashrudin1st@gmail.com>
2025-09-14 14:59:08 +0700

    Complete tugas Individu 3

--- main/models.py
+++ main/models.py
@@ -10,7 +10,7 @@ class Product(models.Model):
     ]
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     price = models.IntegerField()
     stock = models.IntegerField(default=0)
     description = models.TextField()
