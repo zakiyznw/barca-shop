02e706e22a2f5e94e676b9956333f5f0dfb5d124
zakiyznw <zakiynashrudin1st@gmail.com>
2025-09-17 16:41:19 +0700

    Complete tugas Individu 4

--- barca_shop/settings.py
+++ barca_shop/settings.py
@@ -147,10 +147,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
